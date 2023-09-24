27f53403857cbd5807587456d4181cdf3ec09fc6
jeff <jeff.etale@student.moringaschool.com>
2023-09-24 13:23:45 +0300

    used migrations to create database

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,3 +10,7 @@ api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 
 db = SQLAlchemy(app)
+
+from lib.models import Restaurant, Pizza, RestaurantPizza
+
+migrate = Migrate(app, db)
