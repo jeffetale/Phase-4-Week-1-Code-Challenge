98f2331c86d34c83156c43600ad9267f74fae545
jeff <jeff.etale@student.moringaschool.com>
2023-09-24 14:11:49 +0300

    used mpopulated database with seed.py

---
+++ lib/seed.py
@@ -0,0 +1,38 @@
+from faker import Faker
+from lib.models import db, Restaurant, Pizza, RestaurantPizza
+from random import randint
+from lib import app
+
+fake = Faker()
+
+with app.app_context():
+
+    RestaurantPizza.query.delete()
+    Pizza.query.delete()
+    Restaurant.query.delete()
+
+    for r in range(10):
+        restaurant = Restaurant(
+            name = fake.company(),
+            address = fake.address()
+        )
+        db.session.add(restaurant)
+
+    for p in range(10):
+        pizza = Pizza(
+            name = fake.word(ext_word_list = ['Margherita', 'Pepperoni', 'Hawaiian', 'BBQ Chicken', 'Chicken Periperi', 'Chicken Tikka', 'Boerewors']),
+            ingredients = fake.sentence(nb_words=10)
+        )
+
+        db.session.add(pizza)
+
+    for rp in range(10):
+        restaurant_pizza = RestaurantPizza(
+            pizza_id =randint(1, 10),
+            restaurant_id = randint(1, 10),
+            price = randint(1, 1000)
+        )
+
+        db.session.add(restaurant_pizza)
+
+    db.session.commit()
